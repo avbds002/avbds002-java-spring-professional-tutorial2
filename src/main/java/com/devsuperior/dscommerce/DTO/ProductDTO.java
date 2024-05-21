edfdab743cd228ca65b207495193842377a15210
Andre Vinicius <avbds2077@gmail.com>
2024-05-21 14:36:21 -0300

    Validation Exception

--- src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
@@ -1,13 +1,25 @@
 package com.devsuperior.dscommerce.DTO;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Name must have between 3 or 80 characters")
+    @NotBlank(message = "Name field is Required!")
     private String name;
+
+    @Size(min = 10, message = "Description must have at least 10 characters")
+    @NotBlank(message = "Description field is Required!")
     private String description;
+
+    @Positive(message = "Price must be a positive number")
     private Double price;
+
     private String imgUrl;
 
     public ProductDTO() {}
