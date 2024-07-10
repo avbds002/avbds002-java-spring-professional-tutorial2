f0e8d8799923e1c5d67286a1e4104cfa300952cd
avbds002 <avbds2077@gmail.com>
2024-07-09 21:58:42 -0300

    Get user logged in, need to refactor some parts of the project, fixed bug involving null pointer of entity.getId()

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,12 +1,17 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.DTO.UserDTO;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -23,5 +28,25 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return userRepository.findByEmail(username);
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException("INVALID USER");
+        }
+    }
+
+
+    //I don't know why but this method is returning myclientid, i'll probably change this later. If u use the email and pass of some User registered in the DB it will work
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User entity = authenticated();
+        return new UserDTO(entity);
     }
 }
